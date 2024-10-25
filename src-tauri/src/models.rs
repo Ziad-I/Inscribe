30c66819ed978d6a9841559f6d2a1e818af06412
Ziad <68874104+Ziad-I@users.noreply.github.com>
2024-10-25 17:52:55 +0300

    fix rust warning due to unused permission error

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -16,8 +16,8 @@ pub enum Error {
     Utf8(#[from] std::str::Utf8Error),
     #[error("invalid path: {0}")]
     InvalidPath(String),
-    #[error("permission denied: {0}")]
-    PermissionDenied(String),
+    // #[error("permission denied: {0}")]
+    // PermissionDenied(String),
 }
 
 #[derive(Serialize)]
@@ -27,7 +27,7 @@ pub enum ErrorKind {
     Io(String),
     Utf8(String),
     InvalidPath(String),
-    PermissionDenied(String),
+    // PermissionDenied(String),
 }
 
 impl serde::Serialize for Error {
@@ -40,7 +40,7 @@ impl serde::Serialize for Error {
             Self::Io(_) => ErrorKind::Io(error_message),
             Self::Utf8(_) => ErrorKind::Utf8(error_message),
             Self::InvalidPath(_) => ErrorKind::InvalidPath(error_message),
-            Self::PermissionDenied(_) => ErrorKind::PermissionDenied(error_message),
+            // Self::PermissionDenied(_) => ErrorKind::PermissionDenied(error_message),
         };
         error_kind.serialize(serializer)
     }
